a49dbeb9015979cbcca3c3ed4de0fb0613ede1fc
ar-sohan <arsohan101@gmail.com>
2026-08-27 20:07:53 +0600

    Admin : staric routes created with dummy content and installed tailwind for this project

---
+++ app/admin/manage_user/page.tsx
@@ -0,0 +1,5 @@
+export default function ManageUsers() {
+  return (
+    <h1>Manage User</h1>
+  );
+}
--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,19 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <h1>Home</h1>
+    <>
+      <h1 className="text-3xl text-center font-bold">Home</h1>
+      <div className="flex flex-col items-center justify-center mt-8 gap-5" >
+        <h1 className="text-2xl font-semibold">latest News</h1>
+        <p className="text-center max-w-2xl mt-4">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod.
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod.
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod.
+        </p>
+        <button className="bg-blue-500 text-white py-2 px-4 rounded hover:bg-blue-600">
+          Learn More
+        </button>
+      </div>
+    </>
   );
 }
